aacb0a39328fa38c542b6ab02da85bcefd772d74
Chris Bailey <shared@cmbwebsites.com>
2026-06-16 13:47:05 +0100

    Coordinator logic

---
+++ coordinator.py
@@ -0,0 +1,39 @@
+"""Data coordinator for BlueStream integration."""
+
+from typing import Any
+
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
+
+from .const import LOGGER
+from .data import BluStreamData
+from .hdmi_matrix.exceptions import (
+    MatrixCommandError,
+    MatrixConnectionError,
+    MatrixTimeoutError,
+)
+
+MAX_FAILURES = 3
+
+class BluStreamCoordinator(DataUpdateCoordinator):
+    """BluStream data update coordinator."""
+
+    config_entry: BluStreamData
+    consecutive_failures: int = 0
+
+    async def _async_update_data(self) -> Any:
+        """Fetch data from the BluStream device."""
+        try:
+            LOGGER.debug("Updating BluStream data")
+            device_info = await self.config_entry.client.get_device_info()
+            self.consecutive_failures = 0  # Reset on successful update
+            return device_info
+        except (MatrixConnectionError, MatrixTimeoutError) as err:
+            self.consecutive_failures += 1
+            LOGGER.error("Error connecting to BluStream device: %s, consecutive failures: %s", err, self.consecutive_failures)
+            if self.consecutive_failures >= MAX_FAILURES:
+                raise UpdateFailed(f"Error connecting to BluStream device: {err}") from err
+        except MatrixCommandError as err:
+            self.consecutive_failures += 1
+            LOGGER.error("Error fetching data from BluStream device: %s, consecutive failures: %s", err, self.consecutive_failures)
+            if self.consecutive_failures >= MAX_FAILURES:
+                raise UpdateFailed(f"Error fetching data from BluStream device: {err}") from err
---
+++ data.py
@@ -0,0 +1,18 @@
+""" Common data types for the BluStream integration. """
+
+from dataclasses import dataclass
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.loader import Integration
+
+from .coordinator import BluStreamCoordinator
+from .hdmi_matrix import HDMIMatrixClient
+
+type BluStreamConfigEntry = ConfigEntry[BluStreamData]
+
+@dataclass
+class BluStreamData:
+    """Data for the BluStream integration."""
+
+    client: HDMIMatrixClient
+    coordinator: BluStreamCoordinator
+    integration: Integration
