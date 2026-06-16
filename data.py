d628834cd67ff87a1182b58713835ba1be900b5a
Chris Bailey <shared@cmbwebsites.com>
2026-06-16 14:46:54 +0100

    Fix circular import error

--- data.py
+++ data.py
@@ -1,18 +1,22 @@
-""" Common data types for the BluStream integration. """
+"""Common data types for the BluStream integration."""
 
 from dataclasses import dataclass
+from typing import TYPE_CHECKING
+
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.loader import Integration
 
-from .coordinator import BluStreamCoordinator
-from .hdmi_matrix import HDMIMatrixClient
+if TYPE_CHECKING:
+    from .coordinator import BluStreamCoordinator
+    from .hdmi_matrix import HDMIMatrixClient
 
-type BluStreamConfigEntry = ConfigEntry[BluStreamData]
+type BluStreamConfigEntry = ConfigEntry["BluStreamData"]
+
 
 @dataclass
 class BluStreamData:
     """Data for the BluStream integration."""
 
-    client: HDMIMatrixClient
-    coordinator: BluStreamCoordinator
+    client: "HDMIMatrixClient"
+    coordinator: "BluStreamCoordinator"
     integration: Integration
